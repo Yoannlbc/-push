0cbe8fc17947c8d1e2b77f18411be9fdf874f789
Yoann Leblanc <yoannleblanc88@gmail.com>
2025-04-23 21:38:48 +0200

    Fix structure in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 
-    protected
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  end
   end
 end
